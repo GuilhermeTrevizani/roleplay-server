e0c7e69851d0572695b29c04f721fd478b9ecd3b
Guilherme Trevizani <tr3v1z4@gmail.com>
2020-10-02 18:02:13 -0300

    Adicionada idade do personagem no !app

--- RoleplayBOT/PublicModule.cs
+++ RoleplayBOT/PublicModule.cs
@@ -66,7 +66,7 @@ namespace RoleplayBOT
                 };
                 x.AddField("Nome", app.Nome, true);
                 x.AddField("Sexo", app.PersonalizacaoDados.sex == 1 ? "Homem" : "Mulher", true);
-                x.AddField("Nascimento", app.DataNascimento.ToShortDateString(), true);
+                x.AddField("Nascimento", $"{app.DataNascimento.ToShortDateString()} ({Math.Truncate((DateTime.Now.Date - app.DataNascimento).TotalDays / 365):N0} anos)", true);
                 x.WithFooter($"Enviada em {app.DataRegistro}");
                 x.Color = Color.DarkRed;
 
